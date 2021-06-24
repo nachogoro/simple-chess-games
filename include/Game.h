6895643faae0788ecea8b8b9d2c4e98785a879ed
NachoGoro <nachogoro.dev@gmail.com>
2021-06-24 05:01:27 +0200

    Yet more API changes

--- include/Game.h
+++ include/Game.h
@@ -1,12 +1,13 @@
 #ifndef GAME_H_AA82C7D6_D956_405F_95B0_8A23678A5041
 #define GAME_H_AA82C7D6_D956_405F_95B0_8A23678A5041
 
-#include "Move.h"
+#include "PlayedMove.h"
 #include "Board.h"
 #include "Piece.h"
 
 #include <boost/optional.hpp>
 
+#include <cstdint>
 #include <vector>
 
 namespace simplechess
@@ -21,6 +22,9 @@ namespace simplechess
 
 	/**
 	 * \brief A representation of a game of chess at a given point.
+	 *
+	 * The class is immutable, so all methods which would change its state
+	 * instead return a new copy of the object with the updated state.
 	 */
 	class Game
 	{
@@ -47,9 +51,12 @@ namespace simplechess
 			 * game. Hence, the history of the resulting \ref Game will not
 			 * necessarily be of much use.
 			 *
+			 * \param fen The representation of the initial position in
+			 * Forsyth-Edwards Notation.
+			 *
 			 * \return The constructed Game.
 			 */
-			static Game createGameFromStartingFen();
+			static Game createGameFromStartingFen(const std::string& fen);
 
 			/**
 			 * \brief Returns the current state of the game.
@@ -59,11 +66,14 @@ namespace simplechess
 			GameState gameState() const;
 
 			/**
-			 * Returns the latest board position of the game.
+			 * Returns the history of the game as a sequence of \ref GameStage.
 			 *
-			 * \return The latest board position of the game.
+			 * The latest reached position, along with the move which reached
+			 * it, are the last element in the vector.
+			 *
+			 * \return The history of the game.
 			 */
-			const Board& currentPosition() const;
+			const std::vector<GameStage>& gameHistory() const;
 
 			/**
 			 * \brief Returns the \ref Color of the player whose turn it is to
@@ -71,13 +81,13 @@ namespace simplechess
 			 *
 			 * \throws IllegalStateException if the game is finished.
 			 *
-			 * \return The player whose it is to play.
+			 * \return The player whose turn it is to play.
 			 */
 			Color currentPlayer() const;
 
 			/**
-			 * \brief Returns all the possible \c Move for a given piece in the
-			 * board.
+			 * \brief Returns all the possible \c PieceMove for a given piece
+			 * in the board.
 			 *
 			 * If the square is empty or does not contain a piece of the player
 			 * whose turn it is to play, an empty set is returned.
@@ -88,7 +98,7 @@ namespace simplechess
 			 * \param square The \ref Square whose piece is being queried.
 			 * \return All the possible moves for the piece.
 			 */
-			std::set<Move> availableMovesForPiece(const Square& square) const;
+			std::set<PieceMove> availableMovesForPiece(const Square& square) const;
 
 			/**
 			 * \brief Returns all available moves for the player whose turn it
@@ -96,7 +106,88 @@ namespace simplechess
 			 *
 			 * \return All the possible moves for the current player.
 			 */
-			std::set<Move> allAvailableMoves() const;
+			std::set<PieceMove> allAvailableMoves() const;
+
+			/**
+			 * \brief Make a move for the player whose turn it is to play.
+			 *
+			 * \throws IllegalStateException in the following circumstances:
+			 * - The Game has already concluded (its state is not
+			 *   GAME_STATE_PLAYING).
+			 * - The \p pieceMove is not a valid move for the current player.
+			 *
+			 * \param pieceMove The desired piece move.
+			 * \param offerDraw \c true if the move is accompanied by an offer
+			 * to draw to the opponent, \c false otherwise.
+			 *
+			 * \return A new copy of the Game in which the specified move has
+			 * been played.
+			 */
+			Game makeMove(
+					const PieceMove& pieceMove,
+					bool offerDraw=false) const;
+
+			/**
+			 * \brief Whether the player whose turn it is to play can claim a
+			 * draw.
+			 *
+			 * A draw can be claimed under the following circumstances:
+			 * - If the opponent offered a draw in its immediately previous
+			 *   move.
+			 * - If the current position has occurred at least three times in
+			 *   the game (defined as: the same pieces occupy the same squares,
+			 *   the same player has the move, the remaining castling rights
+			 *   are the same and the possibility to capture en passant is the
+			 *   same). In this case, the draw can be claimed right before it
+			 *   appears for the third time or when it has just appeared.
+			 * - If no captures or pawn moves have occurred in the last fifty
+			 *   moves.
+			 * - If there is insufficient material for any side to checkmate.
+			 *
+			 * \note The game automatically ends in a draw, without any player
+			 * having to claim it in the following circumstances:
+			 * - Stalemate (the current player is not in check but has no valid
+			 *   moves).
+			 * - Fivefold repetition (the same position has been reached five
+			 *   times in the game).
+			 * - If no captures or pawn moves have occurred in the last
+			 *   seventy-five moves (unless the last move results in
+			 *   checkmate).
+			 *
+			 * \return \c true if the current player can claim a draw, \c false
+			 * otherwise.
+			 */
+			bool canClaimDraw() const;
+
+			/**
+			 * \brief Claim a draw.
+			 *
+			 * \throws IllegalStateException in the following circumstances:
+			 * - The Game has already concluded (its state is not
+			 *   GAME_STATE_PLAYING).
+			 * - The current player cannot claim a draw (\ref canClaimDraw() is
+			 *   \c false)
+			 *
+			 * \return A new copy of the Game, identical to the current one but
+			 * finished as a draw.
+			 */
+			Game claimDraw() const;
+
+			/**
+			 * \brief Resign the game.
+			 *
+			 * \throws IllegalStateException in the following circumstances:
+			 * - The Game has already concluded (its state is not
+			 *   GAME_STATE_PLAYING).
+			 *
+			 * \return A new copy of the Game, identical to the current one but
+			 * finished with the resignation of \p resigningPlayer.
+			 */
+			Game resign(Color resigningPlayer) const;
+
+		private:
+			const GameState mGameState;
+			const std::vector<Step> mHistory;
 
 	}
 }
---
+++ include/GameStage.h
@@ -0,0 +1,99 @@
+#ifndef GAME_STAGE_H_3064169C_7DBE_4CB3_91C2_EFE730CF43BB
+#define GAME_STAGE_H_3064169C_7DBE_4CB3_91C2_EFE730CF43BB
+
+namespace simplechess
+{
+
+	enum CastlingRight
+	{
+		CASTLING_RIGHT_WHITE_KINGSIDE  = (1 << 0),
+		CASTLING_RIGHT_WHITE_QUEENSIDE = (1 << 1),
+		CASTLING_RIGHT_BLACK_KINGSIDE  = (1 << 2),
+		CASTLING_RIGHT_BLACK_QUEENSIDE = (1 << 3)
+	};
+
+	/**
+	 * \brief A step in the development of a game of chess.
+	 *
+	 * This step is defined as the position of the pieces on the board, plus
+	 * additional context from the game which cannot be inferred from it (which
+	 * color plays the next move, what castling rights remain, etc.), along
+	 * with the move which was last played to achieve the stage.
+	 */
+	class Step
+	{
+		public:
+			/**
+			 * \brief Constructor.
+			 *
+			 * \param board The state of the board at this stage.
+			 * \param toPlay Color whose turn it is to move.
+			 * \param castlingRights A bit mask of the types of \ref
+			 * CastlingRight available in the position.
+			 * \param halfmoveClock Number of half-moves since the last capture
+			 * or pawn advance.
+			 * \param fullmoveClock The number of the full move, starting at 1
+			 * and being incremented after black's move.
+			 * \param move The latest move played to reach this point. Should
+			 * be empty if no move has been played yet.
+			 */
+			Step(const Board& board,
+					Color toPlay,
+					uint8_t castlingRights,
+					uint16_t halfmoveClock,
+					uint16_t fullmoveClock,
+					const boost::optional<PlayedMove>& move);
+
+			/**
+			 * \brief Returns the state of the board in the current stage.
+			 * \return The state of the board in the current stage.
+			 */
+			const Board& board() const;
+
+			/**
+			 * \brief Returns the latest move played at this stage.
+			 * \return The latest move played at this stage.
+			 */
+			const boost::optional<PlayedMove>& move() const;
+
+			/**
+			 * \brief Returns the \ref Color which is to move next.
+			 *
+			 * \return The \ref Color which is to move next.
+			 */
+			Color activeColor() const;
+
+			/**
+			 * \brief Returns a bit mask containing the \ref CastlingRight
+			 * available at this stage.
+			 *
+			 * \return A bit mask containing the \ref CastlingRight
+			 * available at this stage.
+			 */
+			uint8_t castlingRights() const;
+
+			/** \brief Returns the number of half-moves since the last capture
+			 * or pawn advance.
+			 * \return The number of half-moves since the last capture or pawn
+			 * advance.
+			 */
+			uint16_t halfMovesSinceLastCaptureOrPawnAdvance() const;
+
+			/**
+			 * \brief Returns the description of the state of the board in
+			 * Forsyth-Edwards Notation.
+			 * \return The description of the state of the board in FEN format.
+			 */
+			const std::string& fen() const;
+
+		private:
+			const Board mBoard;
+			const Color mActiveColor;
+			const uint8_t mCastlingRights;
+			const uint16_t mHalfmoveClock;
+			const uint16_t mFullmoveClock;
+			const boost::optional<PlayedMove> mMove;
+	};
+}
+
+#endif
